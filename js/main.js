8037b58130069c91cd61dcf3764a10d1272b8985
Marcio Samuel <marciosamuel12@outlook.com>
2021-01-12 22:07:51 -0300

    adiçao do arquivo js do carrossel e criaçao do carrossel no main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ botaoMenu.addEventListener("click", () => {
   document.querySelector("nav").classList.toggle("menu-ativo");
   botaoMenu.classList.toggle("botao-ativo");
 });
+
+const glide = new Glide(".glide", {
+  type: "carousel",
+  autoplay: 5000,
+  perView: 1,
+  focusAt: 'center',
+  gap: 25,
+  hoverpause: true,
+  animationDuration: 600,
+  animationTimingFunc: "linear",
+}).mount();
+
