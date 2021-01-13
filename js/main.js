90a60add5da94257d9b2a9a6bc7ea2b2f910d2a8
Marcio Samuel <marciosamuel12@outlook.com>
2021-01-13 10:08:06 -0300

    scroll da pagina e clique no menu funcionam corretamente

--- js/main.js
+++ js/main.js
@@ -14,6 +14,44 @@ cards.forEach((card) => {
   })
 })
 
+$(document).ready(function () {
+  //Smooth scrolling when click to nav
+  $('.itens-menu a').click(function (e) {
+    e.preventDefault();
+    var curLink = $(this);
+    var scrollPoint = $(curLink.attr('href')).position().top + 1;
+    $('body,html').animate({
+      scrollTop: scrollPoint
+    }, 300);
+  })
+
+  $(window).scroll(function () {
+    onScrollHandle();
+  });
+
+  function onScrollHandle() {
+
+    //Get current scroll position
+    var currentScrollPos = $(document).scrollTop();
+
+    //Iterate through all node
+    $('.itens-menu a').each(function () {
+      var curLink = $(this);
+      var refElem = $(curLink.attr('href'));
+      //Compare the value of current position and the every section position in each scroll
+      if (refElem.position().top <= currentScrollPos && refElem.position().top + refElem.height() > currentScrollPos) {
+        //Remove class link-ativo in all nav
+        $('.navegacao').removeClass("link-ativo");
+        //Add class link-ativo
+        curLink.addClass("link-ativo");
+      }
+      else {
+        curLink.parent().removeClass("link-ativo");
+      }
+    });
+  }
+});
+
 const glide = new Glide(".glide", {
   type: "carousel",
   autoplay: 5000,
@@ -24,4 +62,5 @@ const glide = new Glide(".glide", {
   animationDuration: 600,
   animationTimingFunc: "linear",
 }).mount();
+
 
